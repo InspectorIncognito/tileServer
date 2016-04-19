b32db20a5c4e5226b99886721cb11fe260787d46
Felipe Hernández <cephei.1313@gmail.com>
2016-04-19 10:31:12 -0300

    refactoring variable names

--- install_tile_server.sh
+++ install_tile_server.sh
@@ -4,71 +4,71 @@
 # this file was built following the steps from https://switch2osm.org/serving-tiles/manually-building-a-tile-server-14-04/
 
 # install dependencies
-PASO_1=false
+STEP_1=false
 # install postgreSQL and postgis
-PASO_2=false
+STEP_2=false
 # create database and user
-PASO_3=false
+STEP_3=false
 # create linux user with the same name of postgres user
-PASO_4=false
+STEP_4=false
 # enable postgis on database  
-PASO_5=false
+STEP_5=false
 # install osm2pgsql
-PASO_6=false
+STEP_6=false
 # install mapnik
-PASO_7=false
+STEP_7=false
 # install mod_tile and renderd
-PASO_8=false
+STEP_8=false
 # install stylesheet for map
-PASO_9=false
+STEP_9=false
 # compiling the stylesheet
-PASO_10=false
+STEP_10=false
 # configuring webserver
-PASO_11=false
+STEP_11=false
 # tuning the system
-PASO_12=true
+STEP_12=true
 # Loading data into the server
-PASO_13=true
+STEP_13=true
 # testing server
-PASO_14=true
+STEP_14=true
 
-USER_POSTGRES=transapp
-USER_LINUX=transapp
-DBNAME_POSTGRES=gis
+POSTGRES_USER=transapp
+LINUX_USER=transapp
+POSTGRES_DBNAME=gis
 PATH_SRC=~/Desktop/src
 
 # We will use /usr/local/share/maps/style as a common directory for our stylesheet files and resources.
 PATH_STYLESHEET=/usr/local/share/maps/style
 
 # it is aproximately 700MB 
-if $PASO_1; then 
+if $STEP_1; then 
     echo "PASO 1 ========================================================="
 	sudo apt-get install libboost-all-dev subversion git-core tar unzip wget bzip2 build-essential autoconf libtool libxml2-dev libgeos-dev libgeos++-dev libpq-dev libbz2-dev libproj-dev munin-node munin libprotobuf-c0-dev protobuf-c-compiler libfreetype6-dev libpng12-dev libtiff4-dev libicu-dev libgdal-dev libcairo-dev libcairomm-1.0-dev apache2 apache2-dev libagg-dev liblua5.2-dev ttf-unifont lua5.1 liblua5.1-dev libgeotiff-epsg node-carto 
 fi
 
-if $PASO_2; then
+if $STEP_2; then
     echo "PASO 2 ========================================================="
 	sudo apt-get install postgresql postgresql-contrib postgis postgresql-9.3-postgis-2.1
 fi
 
-if $PASO_3; then
+if $STEP_3; then
     echo "PASO 3 ========================================================="
 	# answer yes for superuser (although this isn't strictly necessary)
-	sudo -u postgres createuser $USER_POSTGRES	
-	sudo -u postgres createdb -E UTF8 -O $USER_POSTGRES $DBNAME_POSTGRES
+	sudo -u postgres createuser $POSTGRES_USER	
+	sudo -u postgres createdb -E UTF8 -O $POSTGRES_USER $POSTGRES_DBNAME
 fi
 
-if $PASO_4; then
+if $STEP_4; then
     echo "PASO 4 ========================================================="
-	sudo useradd -m $USER_LINUX
-	sudo passwd $USER_LINUX # use pass “transapp”	
+	sudo useradd -m $LINUX_USER
+	sudo passwd $LINUX_USER # use pass “transapp”	
 fi
 
-if $PASO_5; then
+if $STEP_5; then
     echo "PASO 5 ========================================================="
-	sudo -u postgres psql -d $DBNAME_POSTGRES -c "CREATE EXTENSION postgis;"
-	sudo -u postgres psql -d $DBNAME_POSTGRES -c "ALTER TABLE geometry_columns OWNER TO $USER_POSTGRES;"
- 	sudo -u postgres psql -d $DBNAME_POSTGRES -c "ALTER TABLE spatial_ref_sys OWNER TO $USER_POSTGRES;"
+	sudo -u postgres psql -d $POSTGRES_DBNAME -c "CREATE EXTENSION postgis;"
+	sudo -u postgres psql -d $POSTGRES_DBNAME -c "ALTER TABLE geometry_columns OWNER TO $POSTGRES_USER;"
+ 	sudo -u postgres psql -d $POSTGRES_DBNAME -c "ALTER TABLE spatial_ref_sys OWNER TO $POSTGRES_USER;"
 fi
 
 # make a directory for dependences's repository 
@@ -76,7 +76,7 @@ echo "CREAR CARPETA SRC =============================================="
 rm -rf $PATH_SRC
 mkdir $PATH_SRC
 
-if $PASO_6; then
+if $STEP_6; then
     echo "PASO 6 ========================================================="
     # install dependencies
     sudo apt-get install make cmake g++ libboost-dev libboost-system-dev libboost-filesystem-dev libexpat1-dev zlib1g-dev libbz2-dev libpq-dev libgeos-dev libgeos++-dev libproj-dev lua5.2 liblua5.2-dev
@@ -91,7 +91,7 @@ if $PASO_6; then
     sudo make install
 fi
 
-if $PASO_7; then
+if $STEP_7; then
     echo "PASO 7 ========================================================="
     cd $PATH_SRC
     git clone git://github.com/mapnik/mapnik
@@ -105,7 +105,7 @@ if $PASO_7; then
     sudo ldconfig
 fi
 
-if $PASO_8; then
+if $STEP_8; then
     echo "PASO 8 ========================================================="
     cd $PATH_SRC
     git clone git://github.com/openstreetmap/mod_tile.git
@@ -119,33 +119,33 @@ if $PASO_8; then
 fi
 
 # it is aproximately 20MB+423MB+ 
-if $PASO_9; then
+if $STEP_9; then
     echo "PASO 9 ========================================================="
     sudo mkdir -p $PATH_STYLESHEET
-    sudo chown $USER_LINUX $PATH_STYLESHEET
+    sudo chown $LINUX_USER $PATH_STYLESHEET
     cd $PATH_STYLESHEET
-    sudo -u $USER_LINUX wget https://github.com/mapbox/osm-bright/archive/master.zip -P $PATH_STYLESHEET
-    sudo -u $USER_LINUX wget http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip -P $PATH_STYLESHEET
-    sudo -u $USER_LINUX wget http://data.openstreetmapdata.com/land-polygons-split-3857.zip -P $PATH_STYLESHEET
-    sudo -u $USER_LINUX mkdir ne_10m_populated_places_simple
+    sudo -u $LINUX_USER wget https://github.com/mapbox/osm-bright/archive/master.zip -P $PATH_STYLESHEET
+    sudo -u $LINUX_USER wget http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip -P $PATH_STYLESHEET
+    sudo -u $LINUX_USER wget http://data.openstreetmapdata.com/land-polygons-split-3857.zip -P $PATH_STYLESHEET
+    sudo -u $LINUX_USER mkdir ne_10m_populated_places_simple
     cd ne_10m_populated_places_simple
-    sudo -u $USER_LINUX wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places_simple.zip -P $PATH_STYLESHEET/ne_10m_populated_places_simple
-    sudo -u $USER_LINUX unzip ne_10m_populated_places_simple.zip
-    sudo -u $USER_LINUX rm ne_10m_populated_places_simple.zip
+    sudo -u $LINUX_USER wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places_simple.zip -P $PATH_STYLESHEET/ne_10m_populated_places_simple
+    sudo -u $LINUX_USER unzip ne_10m_populated_places_simple.zip
+    sudo -u $LINUX_USER rm ne_10m_populated_places_simple.zip
     cd ..
 
     # move the downloaded data into the osm-bright-master project directory
-    sudo -u $USER_LINUX unzip '*.zip'
-    sudo -u $USER_LINUX mkdir osm-bright-master/shp
-    sudo -u $USER_LINUX mv land-polygons-split-3857 osm-bright-master/shp/
-    sudo -u $USER_LINUX mv simplified-land-polygons-complete-3857 osm-bright-master/shp/
-    sudo -u $USER_LINUX mv ne_10m_populated_places_simple osm-bright-master/shp/
+    sudo -u $LINUX_USER unzip '*.zip'
+    sudo -u $LINUX_USER mkdir osm-bright-master/shp
+    sudo -u $LINUX_USER mv land-polygons-split-3857 osm-bright-master/shp/
+    sudo -u $LINUX_USER mv simplified-land-polygons-complete-3857 osm-bright-master/shp/
+    sudo -u $LINUX_USER mv ne_10m_populated_places_simple osm-bright-master/shp/
 
     # to improve performance, we create index files for the larger shapefiles
     cd osm-bright-master/shp/land-polygons-split-3857
-    sudo -u $USER_LINUX shapeindex land_polygons.shp
+    sudo -u $LINUX_USER shapeindex land_polygons.shp
     cd ../simplified-land-polygons-complete-3857/
-    sudo -u $USER_LINUX shapeindex simplified_land_polygons.shp
+    sudo -u $LINUX_USER shapeindex simplified_land_polygons.shp
     cd ../..
 
     # configuring OSM Bright
@@ -154,47 +154,47 @@ if $PASO_9; then
     
     URL_SLP="\"file\": \"http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip\","
     NEW_URL_SLP="\"file\": \"/usr/local/share/maps/style/osm-bright-master/shp/simplified-land-polygons-complete-3857/simplified_land_polygons.shp\", \"type\": \"shape\","
-    sudo -u $USER_LINUX sed -in "s|$URL_SLP|$NEW_URL_SLP|" osm-bright/osm-bright.osm2pgsql.mml
+    sudo -u $LINUX_USER sed -in "s|$URL_SLP|$NEW_URL_SLP|" osm-bright/osm-bright.osm2pgsql.mml
 
     URL_LPS="\"file\": \"http://data.openstreetmapdata.com/land-polygons-split-3857.zip\""
     NEW_URL_LPS="\"file\": \"/usr/local/share/maps/style/osm-bright-master/shp/land-polygons-split-3857/land_polygons.shp\", \"type\": \"shape\","
-    sudo -u $USER_LINUX sed -in "s|$URL_LPS|$NEW_URL_LPS|" osm-bright/osm-bright.osm2pgsql.mml
+    sudo -u $LINUX_USER sed -in "s|$URL_LPS|$NEW_URL_LPS|" osm-bright/osm-bright.osm2pgsql.mml
 
     URL_PPS="\"file\": \"http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.4.0/cultural/10m-populated-places-simple.zip\""
     NEW_URL_PPS="\"file\": \"/usr/local/share/maps/style/osm-bright-master/shp/ne_10m_populated_places_simple/ne_10m_populated_places_simple.shp\", \"type\": \"shape\""
-    sudo -u $USER_LINUX sed -in "s|$URL_PPS|$NEW_URL_PPS|" osm-bright/osm-bright.osm2pgsql.mml
+    sudo -u $LINUX_USER sed -in "s|$URL_PPS|$NEW_URL_PPS|" osm-bright/osm-bright.osm2pgsql.mml
 
     # to replace srs and srs-name with unique line
     SOURCE="\"srs\": \"\","
-    sudo -u $USER_LINUX sed -in "s|$SOURCE||" osm-bright/osm-bright.osm2pgsql.mml
+    sudo -u $LINUX_USER sed -in "s|$SOURCE||" osm-bright/osm-bright.osm2pgsql.mml
     SOURCE_NAME="\"srs-name\": \"autodetect\""
     NEW_SOURCE_NAME="\"srs\": \"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs\""
-    sudo -u $USER_LINUX sed -in "s|$SOURCE_NAME|$NEW_SOURCE_NAME|" osm-bright/osm-bright.osm2pgsql.mml
+    sudo -u $LINUX_USER sed -in "s|$SOURCE_NAME|$NEW_SOURCE_NAME|" osm-bright/osm-bright.osm2pgsql.mml
 
 fi
 
-if $PASO_10; then
+if $STEP_10; then
     echo "PASO 10 ========================================================"
     cd $PATH_STYLESHEET/osm-bright-master
-    sudo -u $USER_LINUX cp configure.py.sample configure.py
+    sudo -u $LINUX_USER cp configure.py.sample configure.py
 
     # Change directory
     OLD_PATH="~/Documents/MapBox/project"
     NEW_PATH="/usr/local/share/maps/style"
 
-    sudo -u $USER_LINUX sed -in "s|$OLD_PATH|$NEW_PATH|" configure.py
+    sudo -u $LINUX_USER sed -in "s|$OLD_PATH|$NEW_PATH|" configure.py
 
     # Replace the database name
     OLD_DBNAME="\"osm\""
 
-    sudo -u $USER_LINUX sed -in "s|$OLD_DBNAME|\"$DBNAME_POSTGRES\"|" configure.py
+    sudo -u $LINUX_USER sed -in "s|$OLD_DBNAME|\"$POSTGRES_DBNAME\"|" configure.py
 
-    sudo -u $USER_LINUX ./make.py
+    sudo -u $LINUX_USER ./make.py
     cd ../OSMBright
-    sudo -u $USER_LINUX carto project.mml | sudo -u $USER_LINUX tee OSMBright.xml
+    sudo -u $LINUX_USER carto project.mml | sudo -u $LINUX_USER tee OSMBright.xml
 fi
 
-if $PASO_11; then
+if $STEP_11; then
     echo "PASO 11 ========================================================"
     PATH_RENDERD_FILE=/usr/local/etc/renderd.conf
     
@@ -226,9 +226,9 @@ if $PASO_11; then
 
     # Create the files required for the mod_tile system to run
     sudo mkdir /var/run/renderd
-    sudo chown $USER_LINUX /var/run/renderd
+    sudo chown $LINUX_USER /var/run/renderd
     sudo mkdir /var/lib/mod_tile
-    sudo chown $USER_LINUX /var/lib/mod_tile
+    sudo chown $LINUX_USER /var/lib/mod_tile
 
     MODTILE_FILE="/etc/apache2/conf-available/mod_tile.conf"
     sudo touch $MODTILE_FILE
@@ -246,7 +246,7 @@ if $PASO_11; then
     sudo service apache2 reload
 fi
 
-if $PASO_12; then
+if $STEP_12; then
     echo "PASO 12 ========================================================"
     POSTGRES_CONF=/etc/postgresql/9.3/main/postgresql.conf
     
@@ -276,12 +276,12 @@ if $PASO_12; then
     #sudo sysctl kernel.shmmax
 fi
 
-if $PASO_13; then
+if $STEP_13; then
     echo "PASO 13 ========================================================"
 
     DATA_PATH=/usr/local/share/maps/planet
     sudo mkdir $DATA_PATH
-    sudo chown $USER_LINUX $DATA_PATH
+    sudo chown $LINUX_USER $DATA_PATH
     cd $DATA_PATH
 
     # get Chile data
@@ -293,10 +293,10 @@ if $PASO_13; then
 fi
 
 # turn on server
-if $PASO_14; then
+if $STEP_14; then
     echo "PASO 13 ========================================================"
 
-    sudo -u $USER_LINUX  renderd -f -c /usr/local/etc/renderd.conf
+    sudo -u $LINUX_USER  renderd -f -c /usr/local/etc/renderd.conf
 
     sudo service apache2 reload
 
